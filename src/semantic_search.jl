72791e996ac9aef41c2d748875f6398ecaa2c7eb
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-10-24 21:06:56 +0200

    Implemented Arora et. al document vectors; improvements in score calculation

--- src/semantic_search.jl
+++ src/semantic_search.jl
@@ -1,21 +1,3 @@
-#TODO(Corneliu) EmbeddedDocument Interface
-#   - EmbeddedDocument <:AbstractDocument
-#   - two fiedls: metadata and embeddings (a single vector of numbers)
-#   - constructor: EmbeddedDocument(<:ConceptNet, (string/tokens/ngrams), embedding_method::Symbol
-#   - methods to convert any existing AbstractDocument to EmbeddedDocument
-#   - A corpus of N embedded documents can be dumped as a vector of metadata and a matrix N×m (m - embedding dim)
-#   - Semantic search in this case would embed the query (using embedding_method)
-#     and multiply it by the matrix: N×m⋅m×1 --> a vector N×1 which after sorting represents the matches
-# ------------------------------------------------------------------------------------------------------------
-#
-# Things to investigate here:
-#  - methods to construct the embedding for a Document
-#  - methods to get as many words as possible given the fact that mispellings may occur
-#  - what words can be safely removed from document/metadata/query without losing match quality
-#  - performance increases through dimensionality reduction and using SparseArrays with vector component
-#    clamping and renormalization
-
-
 # Searcher structures
 mutable struct SemanticCorpusSearcher{T<:AbstractId,
                                       D<:AbstractDocument} <: AbstractSearcher
@@ -59,7 +41,7 @@ function semantic_corpora_searchers(filename::AbstractString,
                                     cptnetpath::AbstractString;
                                     languages=[Languages.English()])
     crefs = parse_corpora_configuration(filename)
-    cptnet = ConceptnetNumberbatch.load_embeddings(cptnetpath, languages=languages)
+    cptnet = load_embeddings(cptnetpath, languages=languages)
     semantic_corpora_searchers(crefs, cptnet)
 end
 
@@ -80,40 +62,64 @@ end
 
 
 # Function to get from multiple word-embeddings to a document embedding
-function get_document_embedding(conceptnet::ConceptNet{L,K,U}, doc) where
+# Through the embedding method, the algorithm for combining multiple word wmbeddings
+# into a single document embedding is controlled. Avalilable options:
+#   :mean - calculate document embedding as the mean of the word embeddings
+#   :arora - algorithm from [1] "A simple but tough-to-beat baseline for sentence embeddings",
+#           Arora et al. ICLR 2017 (https://openreview.net/pdf?id=SyK00v5xx)
+#           [not working properly unless full documents are used]
+function get_document_embedding(conceptnet::ConceptNet{L,K,U}, lexicon, doc;
+                                embedding_method::Symbol=:mean) where
         {L<:Languages.Language, K<:AbstractString, U<:AbstractVector}
     # Text extraction methods various types of documents
-    extract_text(doc::NGramDocument) = collect(keys(doc.ngrams))
-    extract_text(doc::StringDocument) = doc.text
-    extract_text(doc::AbstractString) = doc
+    _extract_tokens(doc::NGramDocument) = collect(keys(doc.ngrams))
+    _extract_tokens(doc::StringDocument) = tokenize_for_conceptnet(doc.text)
+    _extract_tokens(doc::AbstractString) = tokenize_for_conceptnet(doc)
+    # Function that creates a single mean vector from a vector or matrix
+    squash(m) = begin
+        # Calculate document embedding
+        ##############################
+        v = vec(mean(m, dims=2))
+        return v./(norm(v,2)+eps())
+    end
 
-    doc_embs, _ = embed_document(conceptnet,
-                                 extract_text(doc),
-                                 keep_size=false,
-                                 max_compound_word_length=2,
-                                 wildcard_matching=true,
-                                 print_matched_words=false)
+    # Tokenize
+    tokens = _extract_tokens(doc)
+    # Get word embeddings
+    doc_embs, missing_tokens = embed_document(conceptnet,
+                                              tokens,
+                                              keep_size=false,
+                                              max_compound_word_length=1,
+                                              wildcard_matching=true,
+                                              print_matched_words=false)
+    @debug "Document Embedding: $(tokens[missing_tokens]) could not be embedded."
 
-    # TODO: Language detection wold go here :)
+    ############################################
+    # TODO: Language detection wold go here :) #
+    ############################################
 
-    # Return a single document vector
-    if isempty(doc_embs)
-        return zeros(size(conceptnet))
-    else 
-        return squash_embeddings(float(doc_embs))
-    end 
-end
-
-function squash_embeddings(embs::V) where V<:AbstractArray{<:Real}
     # Calculate document embedding
-    ##############################
-    #@inbounds @simd for i in 1:size(embs,2)
-    #    col = view(embs, :, i)
-    #    embs[:,i] = col./(norm(col,1)+eps())
-    #end
-    v = vec(mean(embs, dims=2))
-    return v./(norm(v,1)+eps())
+    n = size(conceptnet,1)
+    isempty(doc_embs) && return zeros(n)
+    em = float(doc_embs)
+    if embedding_method ==:arora
+        # Calculate term frequency vector p
+        tt = sum(values(lexicon))
+        p = [get(lexicon,tk,eps())/tt for (i, tk) in enumerate(tokens) if !(i in missing_tokens)]
+        a = 1
+        # There are no sentences, assuming 1-word sentences
+        for i in 1:length(p)
+            em[:,i] = em[:,i].*(a/(a+p[i]))  # equivalent of vₛ
+        end
+        u, _, _ = svd(em)
+        u = u[:,1]
+        for i in 1:length(p)
+            em[:,i] -= u'u*em[:,i]
+        end
+    end
+    return squash(em)
 end
+
 
 
 function semantic_corpus_searcher(cref, conceptnet::ConceptNet{L,K,U}) where
@@ -124,8 +130,8 @@ function semantic_corpus_searcher(cref, conceptnet::ConceptNet{L,K,U}) where
     prepare!(crps, TEXT_STRIP_FLAGS)
     prepare!(crps_meta, METADATA_STRIP_FLAGS)
     ### # Update lexicons
-    ### update_lexicon!(crps)
-    ### update_lexicon!(crps_meta)
+    update_lexicon!(crps)
+    update_lexicon!(crps_meta)
     ### # Update inverse indices
     ### update_inverse_index!(crps)
     ### update_inverse_index!(crps_meta)
@@ -136,9 +142,11 @@ function semantic_corpus_searcher(cref, conceptnet::ConceptNet{L,K,U}) where
                                  cref,
                                  Dict{Symbol, Matrix{Float64}}())
     # Update CorpusSearcher
-    push!(scs.embeddings, :index=>hcat((get_document_embedding(conceptnet, doc)
+    push!(scs.embeddings, :index=>hcat((get_document_embedding(
+                                            conceptnet, crps.lexicon, doc)
                                         for doc in crps)...))
-    push!(scs.embeddings, :metadata=>hcat((get_document_embedding(conceptnet, doc)
+    push!(scs.embeddings, :metadata=>hcat((get_document_embedding(
+                                                conceptnet, crps_meta.lexicon, doc)
                                            for doc in crps_meta)...))
     return scs
 end
@@ -196,10 +204,13 @@ function search(semantic_crps_searcher::SemanticCorpusSearcher{T,D},
                              [:index, :metadata],
                              [search_type])
     document_scores = zeros(Float64, n)     # document relevance
-    _query = get_document_embedding(conceptnet, query)
+    query_embedding = get_document_embedding(conceptnet,
+                                    semantic_crps_searcher.corpus.lexicon,
+                                    query)
     for wts in where_to_search
         # search
-        document_scores += search(_query, semantic_crps_searcher.embeddings[wts])
+        document_scores += search(query_embedding,
+                                  semantic_crps_searcher.embeddings[wts])
     end
     # Process documents found (sort by score)
     documents_ordered::Vector{Tuple{Float64, Int}} =
@@ -217,6 +228,7 @@ function search(semantic_crps_searcher::SemanticCorpusSearcher{T,D},
 end
 
 function search(query::Vector{N}, embeddings::Matrix{N}) where N<:AbstractFloat
+    # Cosine similarity
     embeddings'*query
 end
 
