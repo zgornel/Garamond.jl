e7d32ba711bc2b2a544bc0ca682b6c9678ad3f85
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-11-07 21:00:39 +0100

    Small fixes for query preparation

--- src/query.jl
+++ src/query.jl
@@ -1,6 +1,6 @@
 # Functionality related to the processing of the query
 """
-    process_query(query [;doc_type=TextAnalysis.NGramDocument])
+    process_query(query [;doc_type=TextAnalysis.StringDocument])
 
 Processes a document `query` and returns a vector of strings
 containing the pre-processed and stemmed tokens.
@@ -8,20 +8,22 @@ Through `doc_type` the type of document in which the query can
 be wrapped (for processing) can be specified, in case the query
 is not already a `TextAnalysis.AbstractDocument`.
 """
-process_query(query::AbstractDocument) = begin
-    prepare!(query, QUERY_STRIP_FLAGS) 
-    stem!(query)
+process_query(query::AbstractDocument;
+              prepare::Bool=true,
+              stem::Bool=true) = begin
+    prepare && prepare!(query, QUERY_STRIP_FLAGS)
+    stem && stem!(query)
     extract_tokens(query)
 end
 
 process_query(query::AbstractString;
-              doc_type::Type{T}=TextAnalysis.NGramDocument
+              doc_type::Type{T}=TextAnalysis.StringDocument
              ) where T<:TextAnalysis.AbstractDocument = begin
     process_query(doc_type(doc))
 end
 
 process_query(query::V;
-              doc_type::Type{T}=TextAnalysis.NGramDocument
+              doc_type::Type{T}=TextAnalysis.StringDocument
              ) where {T<:TextAnalysis.AbstractDocument,
                       V<:AbstractVector{<:AbstractString}} = begin
     process_query(doc_type(join(query," ")))
