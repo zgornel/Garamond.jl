489c0723c3dfd1d46cb00b9555c19ce392e30128
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-11-29 17:15:40 +0100

    Documentation + small refinements to embeddings

--- src/embeddings.jl
+++ src/embeddings.jl
@@ -4,11 +4,21 @@
 abstract type AbstractEmbeddingModel <: AbstractSearchData
 end
 
+"""
+Naive model type for storing text embeddings. It is a wrapper
+around a matrix of embeddings and performs brute search using
+the cosine similarity between vectors.
+"""
 struct NaiveEmbeddingModel{E} <: AbstractEmbeddingModel
     data::Matrix{E}
 end
 
 
+"""
+BruteTree model type for storing text embeddings. It is a wrapper
+around a `BruteTree` NN structure and performs brute search using
+a distance-based similarity between vectors.
+"""
 struct BruteTreeEmbeddingModel{A,D} <: AbstractEmbeddingModel
     tree::BruteTree{A,D}  # Array, Distance and Element types
 end
@@ -17,6 +27,11 @@ BruteTreeEmbeddingModel(data::AbstractMatrix) =
     BruteTreeEmbeddingModel(BruteTree(data))
 
 
+"""
+K-D Tree model type for storing text embeddings. It is a wrapper
+around a `KDTree` NN structure and performs a more efficient
+search using a distance-based similarity between vectors.
+"""
 struct KDTreeEmbeddingModel{A,D} <: AbstractEmbeddingModel
     tree::KDTree{A,D}
 end
@@ -24,6 +39,16 @@ end
 KDTreeEmbeddingModel(data::AbstractMatrix) =
     KDTreeEmbeddingModel(KDTree(data))
 
+
+"""
+HNSW model type for storing text embeddings. It is a wrapper around a
+`HierarchicalNSW` (Hierarchical Navigable Small Worlds [1]) NN graph
+structure and performs a very efficient search using a distance-based
+similarity between vectors.
+[1] Yu. A. Malkov, D.A. Yashunin "Efficient and robust approximate nearest
+    neighbor search using Hierarchical Navigable Small World graphs"
+    (https://arxiv.org/abs/1603.09320)
+"""
 struct HNSWEmbeddingModel{I,E,A,D} <: AbstractEmbeddingModel
     tree::HierarchicalNSW{I,E,A,D}
 end
@@ -41,6 +66,13 @@ end
 
 
 # Nearest neighbor search methods
+"""
+    search(model, point, k)
+
+Searches for the `k` nearest neighbors of `point` in data contained in
+the `model`. The model may vary from a simple wrapper inside a matrix
+to more complex structures such as k-d trees, etc.
+"""
 function search(model::NaiveEmbeddingModel{E}, point::Vector{E}, k::Int) where
         E<:AbstractFloat
     # Cosine similarity
@@ -71,7 +103,6 @@ function search(model::HNSWEmbeddingModel{I,E,A,D}, point::AbstractVector, k::In
 end
 
 
-
 # Length methods
 length(model::NaiveEmbeddingModel) = size(model.data, 2)
 length(model::BruteTreeEmbeddingModel) = length(model.tree.data)
@@ -79,11 +110,15 @@ length(model::KDTreeEmbeddingModel) = length(model.tree.data)
 length(model::HNSWEmbeddingModel) = length(model.tree.data)
 
 
+
 ######################
 # Document Embedding #
 ######################
 """
-Function that creates a single mean vector from a vector or matrix.
+    squash(m)
+
+Function that creates a single mean vector from a matrix `m`
+and performs some normalization operations as well.
 """
 squash(m::Matrix{T}) where T<:AbstractFloat = begin
     # Calculate document embedding
@@ -92,6 +127,13 @@ squash(m::Matrix{T}) where T<:AbstractFloat = begin
     return v./(norm(v,2)+eps(T))
 end
 
+"""
+    squash(vv, m)
+
+Function that creates a single mean vector from a vector of vectors `vv`
+where each vector has a length `m` and performs some normalization
+operations as well.
+"""
 squash(vv::Vector{Vector{T}}, m::Int) where T<:AbstractFloat = begin
     v = zeros(T,m)
     for w in vv
@@ -103,30 +145,15 @@ end
 
 
 """
-    embed_document(embeddings_library, lexicon, document[; embeddings_method])
+    embed_document(embeddings_library, lexicon, document[; embedding_method])
 
 Function to get from multiple sentencea to a document embedding.
 The `embedding_method` option controls how multiple sentence embeddings
-are combined into a single document embedding. Avalilable options:
-    :bow - calculate document embedding as the mean of the sentence embeddings
-    :arora - subtract paragraph/phrase vector [not working properly unless full documents are used]
+are combined into a single document embedding.
+Avalilable options for `embedding_method`:
+    :bow - calculates document embedding as the mean of the sentence embeddings
+    :arora - subtracts paragraph/phrase vector from each sentence embedding
 """
-### function embed_document(embeddings_library,
-###                         lexicon::Dict{String, Int},
-###                         doc::D
-###                        ) where D<:Union{<:AbstractDocument, <:AbstractString}
-###     # Tokenize
-###     tokens = tokenize_fast(doc)
-###     # Get word embeddings
-###     doc_embs, missing_tokens = embed_document(embeddings_library,
-###                                               tokens,
-###                                               keep_size=false,
-###                                               max_compound_word_length=1,
-###                                               wildcard_matching=true,
-###                                               print_matched_words=false)
-###     @debug "Document Embedding: $(tokens[missing_tokens]) could not be embedded."
-###     return float(doc_embs)
-### end
 function embed_document(embeddings_library::Union{
                             ConceptNet{<:Languages.Language, <:AbstractString, T},
                             WordVectors{<:AbstractString, T, <:Integer}},
@@ -168,8 +195,10 @@ end
 
 
 """
+    process_arora(document_embedding, lexicon, embedded_words)
+
 Small function that transforms a document embedding based on word frequencies subtracting the
-`paragraph vector` i.e. principal vector from the word embeddings. Useful for transfer learning
+paragraph vector i.e. principal vector from the word embeddings. Useful for transfer learning
 i.e. use word embeddings trained in a different context than the one where the matching has to
 occur.
 [1] "A simple but tough-to-beat baseline for sentence embeddings", Arora et al. ICLR 2017
@@ -209,15 +238,16 @@ are word embeddings, using the `word2vec` WordVectors object. The function
 has an identical signature as the one from the `ConceptnetNumberbatch`
 package.
 """
-function embed_document(word_vectors::WordVectors,
-                        document_tokens::Vector{S};
+function embed_document(word_vectors::WordVectors{S1,T,H},
+                        document_tokens::Vector{S2};
                         language=Languages.English(),           # not used
                         keep_size::Bool=true,
                         compound_word_separator::String="_",    # not used
                         max_compound_word_length::Int=1,        # not used
                         wildcard_matching::Bool=false,          # not used
                         print_matched_words::Bool=false,
-                       ) where S<:AbstractString
+                       ) where {S1<:AbstractString, T<:Real, H<:Integer,
+                                S2<:AbstractString}
     # Initializations
     n = size(word_vectors)[1]
     p = length(document_tokens)
@@ -233,7 +263,7 @@ function embed_document(word_vectors::WordVectors,
     missing_tokens = setdiff(1:p, found_tokens)  # positions of tokens that were not found
     # Construct document structure
     m = ifelse(keep_size, p, length(found_tokens))
-    embedding = zeros(n, m)
+    embedding = zeros(T, n, m)
     for (pos, tok, j) in zip(found_positions, found_tokens, 1:m)
         embedding[:, ifelse(keep_size, tok, j)] = word_vectors.vectors[:, pos]
     end
