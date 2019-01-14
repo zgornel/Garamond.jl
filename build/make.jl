e9eb2c618a1f532e0740f88c509dfdb4ba67433d
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-01-14 17:26:11 +0100

    Updated build script; removed old Julia compiler, C driver program

--- build/make.jl
+++ build/make.jl
@@ -1,15 +1,106 @@
 #!/bin/julia
 
-# It is assumed that this file is located in <Garamond dirctory>/build
-const BUILD_DIR = abspath(joinpath(@__DIR__, "bin"))
-const GARAMOND_MAIN_FILE = "garamondmain.jl"
-const GARAMOND_MAIN_PATH = abspath(joinpath(@__DIR__, "..", "src",
-                                            GARAMOND_MAIN_FILE))
-JULIAC_JL = abspath(joinpath(@__DIR__, "juliac.jl"))
+# This file must be ran inside the build/ directory of the project. It is assumed that
+# the upper directory i.e. ../ is the main project directory where the Project.toml and
+# Manifest.toml files reside.
+const PROJECT = "\"Garamond\""
+const BASEDIR = @__DIR__                                                # base directory
+const BUILD_DIR = abspath(joinpath(BASEDIR, "bin"))                     # build directory
+const TARGETS = ["gars", "garc", "garw"]                                # files to be compiles
+const TARGETS_PATH = abspath.(joinpath.(BASEDIR, "..", "src", TARGETS)) # full path to the targes
+# Packages that need be specified by url, revision (i.e. unregistered, custom branches)
+const REQUIRED_PACKAGES = ["SnoopCompile", "PackageCompiler"]           # Base required packages
+const PACKAGE_REVS = Dict("HSWN"=>("https://github.com/zgornel/HNSW.jl", "master"),
+                          "Word2Vec"=>("https://github.com/zgornel/Word2Vec.jl", "latest"))
+
+
+##############
+### Checks ###
+##############
+
+# Check that the upper directory has a Project.toml and the project is Garamond
+project_file = joinpath(BASEDIR, "..", "Project.toml")
+if isfile(project_file)
+    try
+        key_name, project_name = strip.(split(readline(project_file), "="))
+    catch
+        @warn "Something went wrong while parsing the first line of $project_file. Exiting..."
+        exit()
+    end
+    if key_name != "name" || project_name != PROJECT
+        @warn "Malformed first line of Project.toml (expected: name=$PROJECT). Exiting..."
+        exit()
+    end
+else
+    @warn "$project_file for $PROJECT does not exist. Exiting..."
+    exit()
+end
+
+# Check for a manifest file, warn if not present
+manifest_file = joinpath(BASEDIR, "..", "Manifest.toml")
+if !isfile(manifest_file)
+    @warn "$manifest_file for $PROJECT does not exist. Will continue..."
+end
+
+# Check that all targets exist
+for target in TARGETS
+    if !isfile(target)
+        @warn "Could not find $target. Exiting..."
+        exit()
+    end
+end
+@info "Checks complete."
+
+# Build or cleanup directory
+if !isdir(BUILD_DIR)
+    mkdir(BUILD_DIR)
+    @info "Created $BUILD_DIR."
+else
+    rm(BUILD_DIR, recursive=true, force=true)
+    @info "Cleaned up $BUILD_DIR."
+end
+
+
+####################
+### Dependencies ###
+####################
 
 using Pkg
-@show @__DIR__
-Pkg.activate(abspath(joinpath(@__DIR__, "..", ".")))
+try
+    # Add project depedencies to the required ones
+    Pkg.activate("..")
+    for pkg in keys(Pkg.installed())
+        push!(REQUIRED_PACKAGES, pkg)
+    end
+catch e
+    @warn "Could not read main package dependencies! ($e)"
+end
+@info "Dependencies are: $REQUIRED_PACKAGES"
+Pkg.activate()  # reactivate default environment
+for pkg in REQUIRED_PACKAGES
+    if  !(pkg in keys(Pkg.installed()))
+        if pkg in keys(PACKAGE_REVS)
+            # Add custom revision, branch etc
+            url, rev = PACKAGE_REVS[pkg]
+            Pkg.add(PackageSpec(url=url, rev=rev))
+        else
+            # Add registered version
+            Pkg.add(pkg)
+        end
+    end
+end
+@info "Installed dependencies."
+
+
+########################
+### Build Executable ###
+########################
+
 using PackageCompiler
-build_executable(GARAMOND_MAIN_PATH, builddir=BUILD_DIR)
-#run(`julia $JULIAC_JL -vosej $GARAMOND_MAIN_PATH -O 2 -d $BUILD_DIR`)
+COPY_JULIALIBS = false
+for (i, target) in enumerate(TARGETS_PATH)
+    @info "*** Building $(uppercase(TARGETS[i])) ***"
+    build_executable(target, builddir=BUILD_DIR, copy_julialibs=COPY_JULIALIBS)
+end
+
+@info "Build complete."
