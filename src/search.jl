74a12bb50272d99140f937f2e6b6a1a7bbafde85
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-01-29 17:16:54 +0100

    Moved score transform to search function

--- src/search.jl
+++ src/search.jl
@@ -138,13 +138,14 @@ function search(srcher::Searcher{T,D,E,M},
         if !iszero(query_embeddings[wts])
             ### Search
             _idxs, _scores = search(srcher.search_data[wts], query_embeddings[wts], k)
+            score_transform!(_scores)
             ###
             idxs = vcat(idxs, _idxs)
             scores = vcat(scores, _scores)
         end
-        if search_type == :all
-            idxs, scores = merge_indices_and_scores(idxs, scores, k)
-        end
+    end
+    if search_type == :all
+        idxs, scores = merge_indices_and_scores(idxs, scores, k)
     end
     # Construct additional structures
     suggestions = MultiDict{String, Tuple{T, String}}()
@@ -262,4 +263,16 @@ function find_matching_needles(iv::OrderedDict{String, Vector{Int}}, needles::Ve
         end
     end
     return needle_matches, doc_matches
+end
+
+
+# Post-processing score function:
+#   - map distances [0, Inf) --> [-1, 1]
+#TODO(Corneliu) Analylically/empirically adapt alpha do vector dimensionality
+function score_transform!(x::Vector{T}, alpha::T=T(0.5)) where T<:AbstractFloat
+    n = length(x)
+    @inbounds @simd for i in 1:n
+        x[i] = 1 - 2.0*tanh(alpha * x[i])
+    end
+    return x
 end
