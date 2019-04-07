5c5caa38fa21b2c9f87a4e40d840c5a282aea0e7
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-04-07 18:13:43 +0200

    Default is no suggestions

--- src/config/defaults.jl
+++ src/config/defaults.jl
@@ -55,7 +55,7 @@ const DEFAULT_PDFTOTEXT_PROGRAM = "/usr/bin/pdftotext"  # program to convert PDF
 const DEFAULT_DOCUMENT_TYPE = StringAnalysis.NGramDocument{String}  # default document object type
 const DEFAULT_MAX_EDIT_DISTANCE = 2  # maximum edit distance for which to return suggestions
 const DEFAULT_MAX_MATCHES = 1_000  # maximum number of matches that can be retrned
-const DEFAULT_MAX_SUGGESTIONS = 1  # maximum number of overall suggestions
+const DEFAULT_MAX_SUGGESTIONS = 0  # maximum number of overall suggestions
 const DEFAULT_MAX_CORPUS_SUGGESTIONS = 0  # maximum number of suggestions for each corpus
 # DYNAMIC Configuration: These defaults can be through run-time options of the
 #                        Garamond CLI client/server utilities
