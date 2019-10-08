5d190af9825b1b1ddc203084f8fee3ef225a1bd6
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-10-08 16:04:41 +0200

    Additional method for __parse for Symbols

--- src/data/dunderparse.jl
+++ src/data/dunderparse.jl
@@ -2,6 +2,8 @@ __parse(::Type{T}, data::AbstractString) where {T} = convert(T, data)
 
 __parse(::Type{T}, data::AbstractString) where {T<:Number} = parse(T, data)
 
+__parse(::Type{T}, data::AbstractString) where {T<:Symbol} = T(data)
+
 __parse(::Type{Vector{Symbol}}, data::Vector) = Symbol.(data)
 
 __parse(::Type{T}, data::S) where{T,S} = try
