9dbd619ad01d43494c44a7057f09674e8e4234fe
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-11-05 16:05:09 +0100

    Moved query generation req parser inside generate_query

--- src/search/recommenders/search.jl
+++ src/search/recommenders/search.jl
@@ -18,10 +18,33 @@ function generate_query(req::AbstractString, dbdata; recommend_id_key=DEFAULT_DB
     __transform_value_for_search(value::AbstractFloat) = "[$(0.9*value), $(1.1*value)]"
     __transform_value_for_search(value::AbstractString) = "\"" * value * "\""  # simple string, wrap in quotes
 
+
+    # Parse a query generation request
+    function __parse_request(req::AbstractString, dbschema; recommend_id_key=DEFAULT_DB_ID_KEY)
+        # Get target id and fields from request string
+        toks = split(req)
+        _target_id, fields = try
+            (string(toks[1]), Symbol.(toks[2:end]))
+        catch e
+            @debug "Could not parse target id, fields: $e"
+            ("", Symbol[])
+        end
+        _target_id_type = typeof(_target_id)
+
+        # Get target id type
+        for dse in dbschema
+            dse.column === recommend_id_key && (_target_id_type = dse.coltype)
+        end
+
+        # Parse target id
+        target_id = __parse(_target_id_type, _target_id)
+        return target_id, fields
+    end
+
+
     # Initializations
     dbschema = db_create_schema(dbdata)
-    target_id, fields = __parse_query_generation_request(req, dbschema,
-                            recommend_id_key=recommend_id_key)
+    target_id, fields = __parse_request(req, dbschema; recommend_id_key=recommend_id_key)
     columns = getproperty.(dbschema, :column)
 
     # Filter on id, find record
@@ -40,28 +63,4 @@ function generate_query(req::AbstractString, dbdata; recommend_id_key=DEFAULT_DB
     end
     query = join(query_toks, " ")
     return (query=query, id=target_id)
-end
-
-
-function __parse_query_generation_request(req::AbstractString,
-                                          dbschema;
-                                          recommend_id_key=DEFAULT_DB_ID_KEY)
-    # Get target id and fields from request string
-    toks = split(req)
-    _target_id, fields = try
-        (string(toks[1]), Symbol.(toks[2:end]))
-    catch e
-        @debug "Could not parse target id, fields: $e"
-        ("", Symbol[])
-    end
-    _target_id_type = typeof(_target_id)
-
-    # Get target id type
-    for dse in dbschema
-        dse.column === recommend_id_key && (_target_id_type = dse.coltype)
-    end
-
-    # Parse target id
-    target_id = __parse(_target_id_type, _target_id)
-    return target_id, fields
 end
