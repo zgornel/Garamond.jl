473559fe7bf2a4cc2fb6b857fc941e7043a18e3d
Corneliu Cofaru <cornel@oxoaresearch.com>
2020-01-13 17:07:55 +0100

    Naive indexes are Vector{Vector{T}}; fixes #9

--- src/index/naive.jl
+++ src/index/naive.jl
@@ -1,30 +1,40 @@
 """
 Naive index type for storing text embeddings. It is a wrapper
-around a matrix of embeddings and performs brute search using
+around a vector of embeddings and performs brute search using
 the cosine similarity between vectors.
 """
-struct NaiveIndex{E, A<:AbstractMatrix{E}} <: AbstractIndex
+struct NaiveIndex{E, A<:Vector{<:AbstractVector{E}}} <: Garamond.AbstractIndex
     data::A
+end
+
+NaiveIndex(data::AbstractMatrix{E}) where E<:AbstractFloat = begin
+    NaiveIndex([data[:,i] for i in 1:size(data,2)])
 end
 
 
 # Nearest neighbor search method
-function knn_search(index::NaiveIndex{E,A},
+function knn_search(index::NaiveIndex{E},
                     point::AbstractVector,
                     k::Int,
                     keep::Vector{Int}=collect(1:length(index))
-                   ) where {E<:AbstractFloat, A<:AbstractMatrix{E}}
+                   ) where {E<:AbstractFloat}
     # Turn sparse vectors into dense ones
     __densify(v::AbstractVector) = v
     __densify(v::AbstractSparseVector) = Vector(v)
 
-    _k = min(k, length(keep))
+    n = length(keep)
+    _k = min(n, k)
+    scores = zeros(E, n)
+    _point = __densify(point)'
     # Cosine similarity
-    scores = index.data[:, keep]' * __densify(point)
+    @inbounds for i in eachindex(keep)
+        scores[i] = _point * index.data[i]
+    end
+    #scores = index.data[:, keep]' * __densify(point)
     idxs = sortperm(scores, rev=true)[1:_k]
     return keep[idxs], one(E) .- scores[idxs]
 end
 
 
 # Length method
-length(index::NaiveIndex) = size(index.data, 2)
+length(index::NaiveIndex) = length(index.data)
--- src/index/naivefast.jl
+++ src/index/naivefast.jl
@@ -1,6 +1,6 @@
 """
 Naive index type for storing text embeddings. It is a wrapper
-around a matrix of embeddings and performs brute search using
+around a vector of embeddings and performs brute search using
 the cosine similarity between vectors.
 
 It does not however use matrix multiplication but element-wise
@@ -11,25 +11,29 @@ with terms in a lexicon and hence, transforming the vectors
 through random projections, LSA etc. prior to search will
 yield wrong results.
 """
-struct NaiveFastIndex{E, A<:AbstractMatrix{E}} <: AbstractIndex
+struct NaiveFastIndex{E, A<:Vector{<:AbstractVector{E}}} <: Garamond.AbstractIndex
     data::A
 end
 
+NaiveFastIndex(data::AbstractMatrix{E}) where E<:AbstractFloat = begin
+    NaiveFastIndex([data[:,i] for i in 1:size(data,2)])
+end
 
-# Nearest neighbor search method
-function knn_search(index::Garamond.NaiveFastIndex{E,A},
+length(index::NaiveFastIndex) = length(index.data)
+
+
+function knn_search(index::NaiveFastIndex{E},
                     point::AbstractVector,
                     k::Int,
                     keep::Vector{Int}=collect(1:length(index))
-                   ) where {E<:AbstractFloat, A<:AbstractMatrix{E}}
+                    ) where {E<:AbstractFloat}
     n = length(keep)
     scores = zeros(E, n)
     inds = findall(>(0), point)
-    _M = view(index.data, inds, keep)
     @inbounds for i in eachindex(keep)
         for j in eachindex(inds)
-            if _M[j, i] != 0.0
-                scores[i]+= _M[j, i] * point[inds[j]]
+            if index.data[i][inds[j]] != 0.0
+                scores[i]+= index.data[i][inds[j]] * point[inds[j]]
             end
         end
     end
@@ -37,7 +41,3 @@ function knn_search(index::Garamond.NaiveFastIndex{E,A},
     _k = min(n, k)
     return keep[order[1:_k]], one(E) .- scores[order[1:_k]]
 end
-
-
-# Length method
-length(index::NaiveFastIndex) = size(index.data, 2)
