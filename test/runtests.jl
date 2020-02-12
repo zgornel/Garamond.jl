c1f7415a10fbe1da63db95ed28b0fc8f04e2d9c5
Corneliu Cofaru <cornel@oxoaresearch.com>
2020-02-12 18:53:11 +0100

    Sample configs are now generated

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ using Garamond
 
 # TODO(Corneliu): Complete testing framework redo
 include("data/datagenerator.jl")
+include("config/config_generator.jl")
 include("input_parsers.jl")
 include("db.jl")
 
