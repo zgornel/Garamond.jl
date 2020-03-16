d27078195ddeb958f7f0891f96064c78ab0771d7
Corneliu Cofaru <cornel@oxoaresearch.com>
2020-03-16 12:22:26 +0100

    Updated app build script (fixes #25)

--- build/build_apps.jl
+++ build/build_apps.jl
@@ -4,23 +4,30 @@
 # the upper directory i.e. ../ is the main project directory where the Project.toml and
 # Manifest.toml files reside.
 const PROJECT = "\"Garamond\""
-const BASEDIR = @__DIR__                                                # base directory
-const BUILD_DIR = abspath(joinpath(BASEDIR, "bin"))                     # build directory
+const BUILD_DIR = "/" * joinpath(split(@__FILE__, "/")[1:end-1]...)       # root project directory
+const PROJECT_DIR = abspath(joinpath(BUILD_DIR, ".."))
+const BASE_ENV_PATH = joinpath(ENV["HOME"], ".julia", "environments",
+                               "v" * string(Int(Base.VERSION.major)) * "." *
+                                     string(Int(Base.VERSION.minor))
+                              )
+const COMPILED_DIR = "compiled"
+const APPS_DIR = "apps"
+const APPS_PATH = abspath(joinpath(PROJECT_DIR, APPS_DIR))
+const COMPILED_PATH = abspath(joinpath(BUILD_DIR, COMPILED_DIR))        # build directory
 const TARGETS = ["gars", "garc", "garw"]                                # files to be compiles
-const TARGETS_PATH = abspath.(joinpath.(BASEDIR, "..", TARGETS))        # full path to the targes
-const REQUIRED_PACKAGES = ["SnoopCompile", "PackageCompiler"]           # Base required packages
+const TARGETS_PATH = abspath.(joinpath.(APPS_PATH, TARGETS))            # full path to the targes
+const REQUIRED_PACKAGES = ["PackageCompiler"]                           # required packages for compilation
 const PACKAGE_REVS = Dict(                                              # Packag revisions/branches/etc
     #"PackageCompiler"=>("https://github.com/JuliaLang/PackageCompiler.jl", "master"),
     "Languages"=>("https://github.com/JuliaText/Languages.jl", "master")
    )
-
 
 ##############
 ### Checks ###
 ##############
 
 # Check that the upper directory has a Project.toml and the project is Garamond
-project_file = joinpath(BASEDIR, "..", "Project.toml")
+project_file = joinpath(PROJECT_DIR, "Project.toml")
 if isfile(project_file)
     local key_name, project_name
     try
@@ -39,7 +46,7 @@ else
 end
 
 # Check for a manifest file, warn if not present
-manifest_file = joinpath(BASEDIR, "..", "Manifest.toml")
+manifest_file = joinpath(PROJECT_DIR, "Manifest.toml")
 if !isfile(manifest_file)
     @warn "$manifest_file for $PROJECT does not exist. Will continue..."
 end
@@ -47,12 +54,12 @@ end
 @info "Pre-checks complete."
 
 # Build or cleanup directory
-if !isdir(BUILD_DIR)
-    mkdir(BUILD_DIR)
-    @info "Created $BUILD_DIR."
+if !isdir(COMPILED_PATH)
+    mkdir(COMPILED_PATH)
+    @info "Created $COMPILED_PATH."
 else
-    rm(BUILD_DIR, recursive=true, force=true)
-    @info "Cleaned up $BUILD_DIR."
+    rm(COMPILED_PATH, recursive=true, force=true)
+    @info "Cleaned up $COMPILED_PATH."
 end
 
 
@@ -63,16 +70,17 @@ end
 using Pkg
 try
     # Add project depedencies to the required ones
-    Pkg.activate("..")
-    Pkg.update()
-    for pkg in keys(Pkg.installed())
-        push!(REQUIRED_PACKAGES, pkg)
-    end
+    Pkg.activate(PROJECT_DIR)
+    Pkg.instantiate()
+    #for pkg in keys(Pkg.installed())
+    #    push!(REQUIRED_PACKAGES, pkg)
+    #end
 catch e
-    @warn "Could not read main package dependencies! ($e)"
+    @warn "Could not read $PROJECT package dependencies! ($e)"
 end
+
 @info "Dependencies are: $REQUIRED_PACKAGES"
-Pkg.activate()  # reactivate default environment
+Pkg.activate(BASE_ENV_PATH)  # reactivate default environment
 Pkg.update()
 for pkg in REQUIRED_PACKAGES
     if  !(pkg in keys(Pkg.installed()))
@@ -98,7 +106,7 @@ if length(ARGS) != 0 && ARGS[1] == "--deps-only"
 else
     # Check that all targets exist
     for target in TARGETS_PATH
-        if !isfile(target)
+        if !isdir(target)
             @warn "Could not find $target. Exiting..."
             exit()
         end
@@ -106,14 +114,11 @@ else
     @info "Checks complete."
 
     using PackageCompiler
-    COPY_JULIALIBS = false
-    for (i, target) in enumerate(TARGETS_PATH)
-        @info "*** Building $(uppercase(TARGETS[i])) ***"
-        build_executable(target,
-                        builddir=BUILD_DIR,
-                        copy_julialibs=COPY_JULIALIBS,
-                        optimize="2")  # -O2
+    cd(APPS_PATH)
+    Pkg.activate(PROJECT_DIR)
+    for (i, target) in enumerate(TARGETS)
+        @info "*** Building $(uppercase(target)) ***"
+        create_app(target, joinpath(COMPILED_PATH, target), force=true)
     end
-
     @info "Build complete."
 end
